ae8a4b7aa23ecaa29261d619e7fc0a56506e9809
brunomaia11 <brunomaia11@gmail.com>
2020-03-29 20:04:06 -0300

    notificação

--- src/route.js
+++ src/route.js
@@ -6,6 +6,8 @@ import ProviderController from './app/controllers/ProviderController';
 import FileController from './app/controllers/FileController';
 import SessionController from './app/controllers/SessionController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -20,5 +22,9 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 routes.post('/files', upload.single('file'), FileController.store);
 routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+routes.get('/schedules', ScheduleController.index);
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 export default routes;
