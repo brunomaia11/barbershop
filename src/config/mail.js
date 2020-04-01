a7cbcec94f1b4ebfe4c976534454ef8a4b8da866
brunomaia11 <brunomaia11@gmail.com>
2020-03-31 22:58:45 -0300

    cancelamento, envio de email, monitoramento redis

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'abefbdda7f270c',
+    pass: '82d33647532b6e',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
